63818b79a8b8ea79dcf5968f38016bd978363a03
acylai0909 <acylai7709@gmail.com>
2025-09-10 12:31:18 +0600

    Первоначальные файлы для проекта MakeCode

---
+++ main.py
@@ -0,0 +1,19 @@
+def on_overlap(sprite, otherSprite):
+    pass
+sprites.on_overlap(SpriteKind.player, SpriteKind.player, on_overlap)
+ControllerButton 
+animation.run_movement_animation(None, animation.animation_presets(animation.fly_to_center), 2000, False)
+music.ring_tone(Note.C)
+music.rest(BeatFraction.HALF)
+effects.blizzard.start_screen_effect()
+music.set_volume(20)
+def on_score():
+    pass
+info.player1.on_score(100, on_score)
+animation.stop_animation(animation.AnimationTypes.ALL, None)
+if True:
+    pass
+else:
+    pass
+    while True:
+        pass
---
+++ main.ts
@@ -0,0 +1,22 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    
+})
+ControllerButton
+animation.runMovementAnimation(null, animation.animationPresets(animation.flyToCenter), 2000, false)
+music.ringTone(Note.C)
+music.rest(BeatFraction.Half)
+effects.blizzard.startScreenEffect()
+music.setVolume(20)
+info.player1.onScore(100, function on_score() {
+    
+})
+animation.stopAnimation(animation.AnimationTypes.All, null)
+if (true) {
+    
+} else {
+    
+    while (true) {
+        
+    }
+}
+
